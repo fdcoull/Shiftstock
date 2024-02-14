befe720e356ceaed213f1acf640b3a980e90ec3e
F D Coull <58175445+fdcoull@users.noreply.github.com>
2024-02-14 17:14:54 +0000

    Add register temp page

---
+++ resources/views/temp/register.blade.php
@@ -0,0 +1,16 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h2>Register</h2>
+    <form action="/register" method="POST">
+        <input type="text" placeholder="Username" name="username">
+        <input type="text" placeholder="Email" name="email">
+        <input type="password" placeholder="Password" name="password">
+        <input type="Submit" value="Register">
+    </form>
+</body>
+</html>
