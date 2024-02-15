aca95fc87c1ad07b733bd1ebac71708532221a9e
F D Coull <58175445+fdcoull@users.noreply.github.com>
2024-02-15 22:51:54 +0000

    Add login temp page

---
+++ resources/views/temp/login.blade.php
@@ -0,0 +1,15 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h2>Login</h2>
+    <form action="/user/login" method="POST">
+        <input type="text" placeholder="Username" name="username">
+        <input type="password" placeholder="Password" name="password">
+        <input type="Submit" value="Login">
+    </form>
+</body>
+</html>
